da283987ba4c1b0f0b4e51d4d006f996c8af408e
cvergara137 <59424023+cvergara137@users.noreply.github.com>
2021-03-26 18:40:57 -0400

    Added stuff in details page and added a way to get to the list and details page through the navigation.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace BookKeeping.Controllers
             return View();
         }
 
+        public IActionResult BookkeeperList()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ Controllers/ScheduleController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookKeeping.Controllers
+{
+    public class ScheduleController : Controller
+    {
+        public IActionResult BookkeeperList()
+        {
+            return View();
+        }
+
+        public IActionResult BookkeeperDetails()
+        {
+            return View(); 
+        } 
+    }
+}
