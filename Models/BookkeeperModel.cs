c5e99db3b33c10021acfdc16891a515bf99bcea3
PJ Johnson <39510645+Phillyfan625@users.noreply.github.com>
2021-04-02 13:22:20 -0400

    Added more

--- Models/BookkeeperModel.cs
+++ Models/BookkeeperModel.cs
@@ -7,6 +7,15 @@ namespace BookKeeping.Models
 {
     public class BookkeeperModel
     {
+        public int BookKeeperID { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+        public int PayRate { get; set; }
+        public string FirstName { get; set; }
+        public string Image { get; set; }
+
+
 
     }
 }
